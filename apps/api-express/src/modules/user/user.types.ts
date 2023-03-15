10f47536977dbb04c4417d7a3f629d7cc1213311
Dima <zahrebelny@gmail.com>
2023-03-15 22:27:30 +0200

    added workspace and user shared types for api

--- apps/api-express/src/modules/user/user.types.ts
+++ apps/api-express/src/modules/user/user.types.ts
@@ -1,7 +1,15 @@
-export interface User {
-  id: string;
-  name: string;
-  email: string;
+import { NewUser } from '@shared/types';
+
+export interface Password {
   password: string;
+}
+
+export type User = NewUser & Password;
+
+export interface RefreshTokenDB {
+  refresh_token: string;
+}
+
+export interface RefreshToken {
   refreshToken: string;
 }
--- packages/types/src/types.ts
+++ packages/types/src/types.ts
@@ -1,5 +1,18 @@
-export interface WorkspaceResponse {
+export interface Workspace {
   id: number;
   name: string;
   userId: string;
+}
+
+export interface NewUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface User extends NewUser {
+  refreshToken: string;
+  accessToken: string;
+  refreshTokenExpiresIn: number;
+  accessTokenExpiresIn: number;
 }
