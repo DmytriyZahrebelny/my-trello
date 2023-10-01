d44d33f9e63f5376a4abf9f28d57889c5d0ac3b2
Dima <zahrebelny@gmail.com>
2023-10-01 23:25:30 +0300

    add new tsconfigs settings

--- apps/ui-react/src/views/authorization/sign-in/sign-in.services.ts
+++ apps/ui-react/src/views/authorization/sign-in/sign-in.services.ts
@@ -1,9 +1,10 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
-export const validateSignInSchema = yupResolver(
-  yup.object().shape({
-    email: yup.string().required('email field is required').email('email must be a valid email'),
-    password: yup.string().required('password is required').min(8),
-  }),
-);
+const schema = yup.object().shape({
+  email: yup.string().required('email field is required').email('email must be a valid email'),
+  password: yup.string().required('password is required').min(8),
+});
+
+// @ts-expect-error
+export const validateSignInSchema = yupResolver(schema);
