204528ff9c7799ecd73c3f044df452f292f5bd18
Dima <zahrebelny@gmail.com>
2023-10-01 23:13:26 +0300

    replace webpack with vite

---
+++ apps/ui-react/vite.config.ts
@@ -0,0 +1,21 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { checker } from 'vite-plugin-checker';
+import path from 'path';
+
+export default defineConfig({
+  build: { outDir: './build' },
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+  ],
+  server: {
+    port: (process.env as unknown as {PORT: number}).PORT || 3000,
+  },
+  resolve: {
+    alias: {
+      "@core": path.resolve(__dirname, "src/core"),
+      "@views": path.resolve(__dirname, "src/views")
+    }
+  }
+});
